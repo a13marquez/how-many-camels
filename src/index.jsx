e64a1b593cee7067269e5ad56fdde133a4c4fc05
Alvaro Marquez <marquezalvaro13@gmail.com>
2019-08-09 09:42:49 +0200

    Fix index files

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './components';
+import { App } from './components/App';
 
-render(document.getElementById('root'), <App />)
+render( <App />, document.getElementById('root'))
 
