8cf6f5224a416009bb616299a5a939ae2bc0925a
Alvaro Marquez <marquezalvaro13@gmail.com>
2019-08-09 10:05:27 +0200

    Add handle height to game

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -6,22 +6,30 @@ export class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      minAge: 18,
+      minHeight: 140,
       age: 18,
-      height: 140,
+      height: 140
     }
     this.onUpdateAge = this.onUpdateAge.bind(this);
+    this.onUpdateHeight = this.onUpdateHeight.bind(this);
   }
 
   onUpdateAge(age) {
-    this.setState({ age: age })
+    this.setState({ age: age });
+  }
+  onUpdateHeight(height) {
+    this.setState({ height: height });
   }
 
   render() {
+    const { minAge, minHeight } = this.state
     return(
       <div className="section">
-        <Slider label="Age" min={this.state.age} max={100} step={1}
+        <Slider label="Age" min={minAge} max={100} step={1}
           handleChange={this.onUpdateAge}></Slider>
-        <Slider label="Height(cm)" min={this.state.height} max={210} step={1}></Slider>
+        <Slider label="Height(cm)" min={minHeight} max={210} step={1}
+          handleChange={this.onUpdateHeight}></Slider>
       </div>
     )
   }
